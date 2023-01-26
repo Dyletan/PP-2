13178cfea106c9af9a2d7a1e729328f6f54d588e
dyletant <eldar1016@gmail.com>
2023-01-26 14:14:03 +0600

    Three exercises done

---
+++ lab_1/Syntax.py
@@ -0,0 +1,21 @@
+#1
+print("Hello, World!")
+#2
+if 5 > 2:
+    print("Five is greater than two!")
+#3
+#This is a comment
+#4
+"""
+This is a comment
+written in 
+more than just one line
+"""
+#5
+carname = "Volvo"
+#6
+x = 50
+#7
+x = 5
+y = 10
+print(x + y)
---
+++ lab_1/variables.py
@@ -0,0 +1,7 @@
+carname = "Volvo"
+#6
+x = 50
+#7
+x = 5
+y = 10
+print(x + y)
