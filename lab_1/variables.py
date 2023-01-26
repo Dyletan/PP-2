66a5e67d60c85b46340ac409a8c5def8e4c57f3b
dyletant <eldar1016@gmail.com>
2023-01-26 14:23:46 +0600

    added more exercises

--- lab_1/variables.py
+++ lab_1/variables.py
@@ -1,7 +1,21 @@
 carname = "Volvo"
-#6
+#1
 x = 50
-#7
+#2
 x = 5
 y = 10
 print(x + y)
+#3
+x = 5
+y = 10
+z = x + y
+print(z)
+#4
+myfirst_name = "John"
+#5
+x = y = z = "Orange"
+#6
+def myfunc():
+    global x
+    x = "fantastic"
+#7
