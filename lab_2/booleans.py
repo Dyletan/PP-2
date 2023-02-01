7a3c682ceed11ab2ddce872759539a461cfd8cf0
dyletant <eldar1016@gmail.com>
2023-02-01 19:46:15 +0600

    Lab done

---
+++ lab_2/booleans.py
@@ -0,0 +1,10 @@
+# 1 
+print(10 > 9) # value - True
+# 2
+print(10 == 9) # value - False
+# 3
+print(10 < 9) # value - False
+# 4
+print(bool("abc")) # value - True
+# 5
+print(bool(0)) # value - False
